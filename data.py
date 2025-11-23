a99bd44e4eec62090d8ef772e1dda3f7daa921cd
“Kodai <kkodai1209@gmail.com>
2025-11-23 15:47:46 +0900

    add sinkhorn

--- data.py
+++ data.py
@@ -2,18 +2,16 @@ import torch
 from torch.utils.data import Dataset
 
 class TSPDataset(Dataset):
-    def __init__(self, num_samples, num_nodes, seed=None):
+    def __init__(self, num_samples, num_nodes):
         super().__init__()
-        
-        if seed is not None:
-            torch.manual_seed(seed)
-            
         self.num_samples = num_samples
         self.num_nodes = num_nodes
         self.points = torch.rand(num_samples, num_nodes, 2)
         self.distances = torch.cdist(self.points, self.points, p=2)
+    
     def __len__(self):
         return self.num_samples
+    
     def __getitem__(self, idx):
         return {
             'points': self.points[idx],
--- utils.py
+++ utils.py
@@ -3,3 +3,15 @@ import torch
 def get_adjacency_matrix(distance_matrix, scale):
     adjacency_matrix = torch.exp(-distance_matrix / scale)
     return adjacency_matrix
+
+def sample_gumbel(shape):
+    u = torch.rand(shape)
+    return -torch.log(-torch.log(u))
+
+def sinkhorn(logits, n_iters):
+    P = torch.exp(logits)
+    for _ in range(n_iters):
+        P = P / torch.sum(P, dim=-1, keepdim=True)
+        P = P / torch.sum(P, dim=-2, keepdim=True)
+    return P
+    
