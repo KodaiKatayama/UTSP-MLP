968815bc9d1b99f7d6cbc344b7367df7a148e528
“Kodai <kkodai1209@gmail.com>
2025-11-23 16:19:14 +0900

    add Hungarian

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import torch
+from scipy.optimize import linear_sum_assignment
 
 def get_adjacency_matrix(distance_matrix, scale):
     adjacency_matrix = torch.exp(-distance_matrix / scale)
@@ -8,10 +9,21 @@ def sample_gumbel(shape):
     u = torch.rand(shape)
     return -torch.log(-torch.log(u))
 
-def sinkhorn(logits, n_iters):
-    P = torch.exp(logits)
+def sinkhorn(logit, n_iters):
+    T = torch.exp(logit)
     for _ in range(n_iters):
-        P = P / torch.sum(P, dim=-1, keepdim=True)
-        P = P / torch.sum(P, dim=-2, keepdim=True)
+        T = T / torch.sum(T, dim=-1, keepdim=True)
+        T = T / torch.sum(T, dim=-2, keepdim=True)
+    return T
+
+def gumbel_sinkhorn(logit, tau, gamma, n_iters):
+    gumbel_noise = sample_gumbel(logit.shape)
+    noisy_logits = (logit + gamma * gumbel_noise) / tau
+    return sinkhorn(noisy_logits, n_iters)
+
+def Hungarian(logit):
+    logit_np = logit.detach().cpu().numpy()
+    row, col = linear_sum_assignment(logit_np, maximize=True)
+    P = torch.zeros_like(logit)
+    P[row, col] = 1
     return P
-    
