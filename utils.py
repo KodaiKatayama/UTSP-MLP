9155f0f82dee9927476f614c0ec35f94be5dce99
“Kodai <kkodai1209@gmail.com>
2025-11-23 16:31:24 +0900

    add get_cyclic_matrix

--- utils.py
+++ utils.py
@@ -16,14 +16,16 @@ def sinkhorn(logit, n_iters):
         T = T / torch.sum(T, dim=-2, keepdim=True)
     return T
 
-def gumbel_sinkhorn(logit, tau, gamma, n_iters):
-    gumbel_noise = sample_gumbel(logit.shape)
-    noisy_logits = (logit + gamma * gumbel_noise) / tau
-    return sinkhorn(noisy_logits, n_iters)
-
 def Hungarian(logit):
     logit_np = logit.detach().cpu().numpy()
-    row, col = linear_sum_assignment(logit_np, maximize=True)
+    row, col = linear_sum_assignment(logit_np)
     P = torch.zeros_like(logit)
     P[row, col] = 1
     return P
+
+def get_cyclic_matrix(num_nodes):
+    matrix = torch.zeros(num_nodes, num_nodes)
+    for i in range(num_nodes - 1):
+        matrix[i, i + 1] = 1
+    matrix[num_nodes - 1, 0] = 1
+    return matrix
