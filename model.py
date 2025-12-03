c449b4916640b7357af1dc55b5015972aedf6a9e
“Kodai <146795421+KodaiKatayama@users.noreply.github.com>
2025-12-03 13:09:11 +0900

    change

--- model.py
+++ model.py
@@ -20,19 +20,9 @@ class GCN(nn.Module):
     def __init__(self, input_dim, hidden_dim, num_nodes, alpha, **kwargs):
         super().__init__()
         self.alpha = alpha
-        # TODO: GCNのレイヤー定義をここに書く
-        # self.conv1 = ...
         pass
 
     def forward(self, x, adj):
-        """
-        GNN系は adj (隣接行列) を使ってメッセージパッシングを行う
-        """
-        # TODO: 実装
-        # h = self.conv1(x, adj)
-        # ...
-        
-        # ダミー出力: MLPと同様の形状を返す必要がある (Batch, N, N)
         batch_size, n_nodes, _ = x.shape
         raw_logits = torch.zeros(batch_size, n_nodes, n_nodes, device=x.device)
         
@@ -43,11 +33,9 @@ class SAGs(nn.Module):
     def __init__(self, input_dim, hidden_dim, num_nodes, alpha, **kwargs):
         super().__init__()
         self.alpha = alpha
-        # TODO: Scattering Attention Graph Neural Network の定義
         pass
 
     def forward(self, x, adj):
-        # TODO: 実装
         batch_size, n_nodes, _ = x.shape
         raw_logits = torch.zeros(batch_size, n_nodes, n_nodes, device=x.device)
         
@@ -55,9 +43,6 @@ class SAGs(nn.Module):
         return logits
 
 def get_model(model_name, input_dim, hidden_dim, num_nodes, alpha, **kwargs):
-    """
-    モデル名に応じてインスタンスを返すファクトリー関数
-    """
     model_name = model_name.lower()
     
     if model_name == 'mlp':
